1dde91f5c8553d882e3c817b4ad454d88a2a65af
wjf <wjf@wjfdeMacBook-Pro.local>
2021-10-19 22:20:35 +0800

    and

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("这是汪家锋的按钮！");
+                System.out.println("THIS is a button from wjf");
+                Log.d("MainActivity","hello");
+                builder.show();
+            }
+        });
+    }
+}
